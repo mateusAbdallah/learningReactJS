92edb8109475a60f48185c923936a183966a7cef
Mateus Abdallah <abdallahmateus@gmail.com>
2022-10-18 12:28:30 -0700

    testing JSX in App function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function name(first, last){
+    return first + last
+  }
+  
+  function sum(a, b){
+    return a + b
+  }
+  
+
+
   return (
     <div className="App">
       <h1>Hello React</h1>
       <p>My first application with React</p>
+      <p>Olá, {name("Mateus", "Abdallah")}</p>    
+      <p>Soma: {sum(22, 15)} </p>  
     </div>
   );
 }
